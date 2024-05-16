ff3d7110f8a5d4f7375ef8477587e4ff0401695e
torotoro21 <anggaprihantoro3@gmail.com>
2024-05-16 13:24:32 +0700

    feat: jasper report

---
+++ src/main/java/com/lawencon/pss/controller/ReportController.java
@@ -0,0 +1,44 @@
+package com.lawencon.pss.controller;
+
+import java.io.FileNotFoundException;
+import java.util.Base64;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lawencon.pss.service.ReportService;
+
+import lombok.RequiredArgsConstructor;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperPrint;
+
+@RestController
+@RequestMapping("reports")
+@RequiredArgsConstructor
+public class ReportController {
+	
+	private final ReportService reportService;
+	
+	@GetMapping()
+	public ResponseEntity<?> printReport() throws FileNotFoundException, JRException{
+		
+		final JasperPrint jasperPrint = reportService.exportReport();
+		
+		byte fileBytes[] = JasperExportManager.exportReportToPdf(jasperPrint);
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Content-Disposition", "inline; filename=companies.pdf");
+
+		return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=companies.pdf")
+                .body(fileBytes);
+
+	}
+
+}
---
+++ src/main/java/com/lawencon/pss/service/ReportService.java
@@ -0,0 +1,12 @@
+package com.lawencon.pss.service;
+
+import java.io.FileNotFoundException;
+
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperPrint;
+
+public interface ReportService {
+	
+	JasperPrint exportReport() throws FileNotFoundException, JRException;
+
+}
