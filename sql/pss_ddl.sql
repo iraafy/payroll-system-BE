7905bd1bd7b064319ba50dfc37fb22a0a6b7378e
Ahmad Aminullah <ahmad.aminullah@ui.ac.id>
2024-05-17 08:42:10 +0700

    fix: add new column on ddl query

--- sql/pss_ddl.sql
+++ sql/pss_ddl.sql
@@ -14,6 +14,7 @@ CREATE TABLE tb_m_email_templates (
 
 CREATE TABLE tb_m_files (
 	id VARCHAR(36) PRIMARY KEY,
+	file_content TEXT,
 	stored_path TEXT,
 	created_by VARCHAR(36) NOT NULL,
 	created_at TIMESTAMP NOT NULL,
@@ -27,6 +28,7 @@ CREATE TABLE tb_m_companies (
 	id VARCHAR(36) PRIMARY KEY,
 	company_name VARCHAR(50) NOT NULL,
 	logo_id VARCHAR(36),
+	default_payment_day SMALLINT NOT NULL,
 	created_by VARCHAR(36) NOT NULL,
 	created_at TIMESTAMP NOT NULL,
 	updated_by VARCHAR(36),
@@ -101,7 +103,7 @@ CREATE TABLE tb_r_payrolls (
 	id VARCHAR(36) PRIMARY KEY,
 	client_id VARCHAR(36) NOT NULL,
 	title TEXT,
-	scheduled_date TIMESTAMP,
+	scheduled_date TIMESTAMP NOT NULL,
 	created_by VARCHAR(36) NOT NULL,
 	created_at TIMESTAMP NOT NULL,
 	updated_by VARCHAR(36),
@@ -119,13 +121,15 @@ CREATE TABLE tb_r_payroll_details (
 	max_upload_date TIMESTAMP NOT NULL,
 	ps_acknowledge BOOLEAN,
 	client_acknowledge BOOLEAN,
+	for_client Boolean NOT NULL,
 	created_by VARCHAR(36) NOT NULL,
 	created_at TIMESTAMP NOT NULL,
 	updated_by VARCHAR(36),
 	updated_at TIMESTAMP,
 	vrsion INT NOT NULL,
 	is_active BOOLEAN DEFAULT TRUE,
-	CONSTRAINT payroll_fk FOREIGN KEY(payroll_id) REFERENCES tb_r_payrolls(id)
+	CONSTRAINT payroll_fk FOREIGN KEY(payroll_id) REFERENCES tb_r_payrolls(id),
+	CONSTRAINT file_fk FOREIGN KEY(file_id) REFERENCES tb_m_files(id)
 );
 
 CREATE TABLE tb_r_reschedules (
@@ -147,8 +151,8 @@ INSERT INTO tb_m_user_roles (id, role_name, role_code, created_by, created_at, i
 	('1e5b3f28-67c4-4615-b0e0-7e7c90f61608', 'PS', 'PS001', '1', NOW(), TRUE, 0),
 	('8487cf5c-044d-44e9-8e02-e51fd8c8d127', 'Client', 'CLNT1', '1', NOW(), TRUE, 0);
 
-INSERT INTO tb_m_companies (id, company_name, logo_id, created_by, created_at, is_active, vrsion) VALUES
-	('cc8c66d3-96ab-4ecc-bbe9-b984e8562190', 'Lawencon', null, '1', NOW(), TRUE, 0);
+INSERT INTO tb_m_companies (id, company_name, logo_id, default_payment_day, created_by, created_at, is_active, vrsion) VALUES
+	('cc8c66d3-96ab-4ecc-bbe9-b984e8562190', 'Lawencon', null, 31, '1', NOW(), TRUE, 0);
 
 INSERT INTO tb_m_users (id, email, pwd, role_id, company_id, created_by, created_at, is_active, vrsion) VALUES 
     (uuid_generate_v4(), 'a@mail.com', '$2y$10$AMf5FaHHEF5cJFBNEDHl9.4b/QjWSfPULHtJQgqyL2NYsfgkVAyIq', '1e5b3f28-67c4-4615-b0e0-7e7c90f61608', 'cc8c66d3-96ab-4ecc-bbe9-b984e8562190','1', NOW(), TRUE, 0),
@@ -163,6 +167,3 @@ INSERT INTO tb_m_users (id, email, pwd, role_id, company_id, created_by, created
 
 
 DROP TABLE tb_r_reschedules, tb_r_payroll_details, tb_r_payrolls, tb_r_chats, tb_r_client_assignments, tb_m_users, tb_m_user_roles, tb_m_companies, tb_m_files, tb_m_email_templates;
-
-SELECT * FROM tb_m_users tmu ;
-SELECT * FROM tb_r_client_assignments trca ;
