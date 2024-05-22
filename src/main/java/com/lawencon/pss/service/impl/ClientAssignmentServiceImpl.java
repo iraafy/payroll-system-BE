68abae1a67e69c5d1f32f7906faf9375d6e83a7e
torotoro21 <anggaprihantoro3@gmail.com>
2024-05-22 11:57:20 +0700

    fix: tambah condisional checking on company logo id

--- src/main/java/com/lawencon/pss/service/impl/ClientAssignmentServiceImpl.java
+++ src/main/java/com/lawencon/pss/service/impl/ClientAssignmentServiceImpl.java
@@ -39,7 +39,9 @@ public class ClientAssignmentServiceImpl implements ClientAssignmentService {
 			client.setId(clientUser.getId());
 			client.setCompany(company.getCompanyName());
 			client.setName(clientUser.getFullName());
-			client.setFileId(company.getLogoId().getId());
+			if(company.getLogoId() != null) {
+				client.setFileId(company.getLogoId().getId());				
+			}
 
 			response.add(client);
 		}
