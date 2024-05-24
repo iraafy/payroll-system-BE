c5391b3fa16cc57da5e7b0b651088ea6d389a097
Ahmad Aminullah <ahmad.aminullah@ui.ac.id>
2024-05-24 09:42:15 +0700

    fix: model column name and add anotation on notif repository

--- src/main/java/com/lawencon/pss/repository/NotificationRepository.java
+++ src/main/java/com/lawencon/pss/repository/NotificationRepository.java
@@ -3,9 +3,11 @@ package com.lawencon.pss.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.lawencon.pss.model.Notification;
 
+@Repository
 public interface NotificationRepository extends JpaRepository<Notification, String>{
 
 	List<Notification> findAll();
