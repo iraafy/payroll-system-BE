6823678e4c3b0e27153b996f591cee830eeba785
Ahmad Aminullah <ahmad.aminullah@ui.ac.id>
2024-05-27 13:14:49 +0700

    fix: minor bug

--- src/main/java/com/lawencon/pss/service/impl/EmailServiceImpl.java
+++ src/main/java/com/lawencon/pss/service/impl/EmailServiceImpl.java
@@ -1,25 +1,26 @@
 package com.lawencon.pss.service.impl;
 
-import com.lawencon.pss.service.EmailService;
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.Map;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.context.Context;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 
-import javax.mail.MessagingException;
-import javax.mail.internet.MimeMessage;
-import java.util.Map;
+import com.lawencon.pss.service.EmailService;
+
+import lombok.RequiredArgsConstructor;
 
 @Service
+@RequiredArgsConstructor
 public class EmailServiceImpl implements EmailService {
 
-    @Autowired
-    private JavaMailSender emailSender;
-
-    @Autowired
-    private SpringTemplateEngine templateEngine;
+    private final JavaMailSender emailSender;
+    private final SpringTemplateEngine templateEngine;
 
     @Override
     public void sendEmail(String to, String subject, String templateName, Map<String, Object> templateModel) throws MessagingException {
