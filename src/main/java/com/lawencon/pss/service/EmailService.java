3a461247d8cb5cd6380afbecc2c751b8366a9e0f
iraafy <irafitriyani62@gmail.com>
2024-05-27 11:46:50 +0700

    create template email

--- src/main/java/com/lawencon/pss/service/EmailService.java
+++ src/main/java/com/lawencon/pss/service/EmailService.java
@@ -1,7 +1,11 @@
 package com.lawencon.pss.service;
 
+import java.util.Map;
+
+import javax.mail.MessagingException;
+
 public interface EmailService {
 	
-	void sendEmail(String to, String subject, String body);
-
+    void sendEmail(String to, String subject, String templateName, Map<String, Object> templateModel) throws MessagingException;
+    
 }
