d636a7c064a3184125c9cd266de583a40e659413
Ahmad Aminullah <ahmad.aminullah@ui.ac.id>
2024-05-30 09:45:55 +0700

    fix: add list for get ps name

--- src/main/java/com/lawencon/pss/model/User.java
+++ src/main/java/com/lawencon/pss/model/User.java
@@ -1,9 +1,13 @@
 package com.lawencon.pss.model;
 
+import java.util.List;
+import java.util.Set;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import lombok.Getter;
@@ -35,5 +39,8 @@ public class User extends BaseModel {
     @ManyToOne
     @JoinColumn(name = "company_id")
     private Company company;
+    
+//    @OneToMany(mappedBy = "ps")
+//    private Set<ClientAssignment> pics;
 
 }
