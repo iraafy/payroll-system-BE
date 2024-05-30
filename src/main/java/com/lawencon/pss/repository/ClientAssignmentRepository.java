cb2daf8b83b38cd73b934f8fb84cb066cc075244
Ahmad Aminullah <ahmad.aminullah@ui.ac.id>
2024-05-30 14:08:20 +0700

    fix: custom query

--- src/main/java/com/lawencon/pss/repository/ClientAssignmentRepository.java
+++ src/main/java/com/lawencon/pss/repository/ClientAssignmentRepository.java
@@ -4,8 +4,6 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.lawencon.pss.model.ClientAssignment;
@@ -14,14 +12,7 @@ import com.lawencon.pss.model.ClientAssignment;
 public interface ClientAssignmentRepository extends JpaRepository<ClientAssignment, String>{
 
 	List<ClientAssignment> findByPsId(String id);
-	Optional<ClientAssignment> findByClientId(String id);
 	
-	@Query(value = 
-    		"SELECT ca "
-    		+ "FROM ClientAssignment AS ca "
-    		+ "RIGHT JOIN User AS u ON ca.client.id = u.id "
-    		+ "RIGHT JOIN User AS u2 ON ca.ps.id = u2.id "
-    		+ "WHERE u.role.roleCode = :roleCode;"
-    		)
-    List<ClientAssignment> findClientWithPs(@Param("roleCode") String roleCode);
+	Optional<ClientAssignment> findByClientId(String id);
+
 }
