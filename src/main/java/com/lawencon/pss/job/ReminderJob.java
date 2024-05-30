019eea4ecfb139e351f7bdccbbce7089fbb43d9e
Ahmad Aminullah <ahmad.aminullah@ui.ac.id>
2024-05-30 09:29:14 +0700

    feat: job for sending reminder email

--- src/main/java/com/lawencon/pss/job/ReminderJob.java
+++ src/main/java/com/lawencon/pss/job/ReminderJob.java
@@ -1,6 +1,12 @@
 package com.lawencon.pss.job;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.mail.MessagingException;
+
 import org.quartz.Job;
+import org.quartz.JobDataMap;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.springframework.stereotype.Component;
@@ -17,8 +23,25 @@ public class ReminderJob implements Job {
 	
 	@Override
 	public void execute(JobExecutionContext context) throws JobExecutionException {
-		// TODO Auto-generated method stub
+		final Runnable runnable = () -> {
+			final JobDataMap jobDataMap = context.getJobDetail().getJobDataMap();
+			final ReminderData data = (ReminderData) jobDataMap.get(ReminderJob.class.getSimpleName());
+			final String subject = "Pengingat - " + data.getMessage();
+			final String email = data.getEmail();
+			final Map<String, Object> templateModel = new HashMap<>();
+			templateModel.put("fullName", data.getFullName());
+			templateModel.put("message", data.getMessage());
+			templateModel.put("activityLink", data.getActivityLink());
+			
+			try {
+				emailService.sendTemplateEmail(email, subject, "reminder-email", templateModel);
+			} catch (MessagingException e) {
+				e.printStackTrace();
+			}
+		};
 		
+		final Thread mailThread = new Thread(runnable);
+		mailThread.start();
 	}
 
 	
